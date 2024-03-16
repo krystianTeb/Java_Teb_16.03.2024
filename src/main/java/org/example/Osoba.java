d007abe6b9007a15754bf4cac9528215eefcaf1f
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-16 08:38:30 +0100

    1

---
+++ src/main/java/org/example/Osoba.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Osoba {
+    //Właściwości/pola/atrybuty - opisują cechy klasy
+    public String imie;
+    public String nazwisko;
+    public String pesel;
+}
