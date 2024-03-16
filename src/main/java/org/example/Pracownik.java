ea00b944feb65a38b6fa203813a2712128cf4110
Krystian <krystian.lewandowski@teb.edu.pl>
2024-03-16 12:41:30 +0100

    10_lombok

--- src/main/java/org/example/Pracownik.java
+++ src/main/java/org/example/Pracownik.java
@@ -1,37 +1,11 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
 public class Pracownik {
     private String imie;
     private String nazwisko;
     private EDzial dzial;
-
-    public Pracownik(String imie, String nazwisko, EDzial dzial) {
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.dzial = dzial;
-    }
-
-    public String getNazwisko() {
-        return nazwisko;
-    }
-
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
-    }
-
-    public String getImie() {
-        return imie;
-    }
-
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
-    public EDzial getDzial() {
-        return dzial;
-    }
-
-    public void setDzial(EDzial dzial) {
-        this.dzial = dzial;
-    }
 }
